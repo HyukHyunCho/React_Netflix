f74af143b2c5b3f2981226e8957594fdb3b101c0
HyukHyunCho <gurgus0189@gmail.com>
2023-01-19 06:19:22 +0900

    chore: 스타일 수정 및 이벤트 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,44 @@
-import React from 'react'
-import Navbar from "../components/Navbar/Navbar";
+import React, { useState } from 'react'
+import styled from "styled-components";
 import Banner from "../components/Banner/Banner";
 import Row from "../components/Row/Row";
-import Footer from "../components/Footer/Footer";
+import MovieModal from '../components/Modal/MovieModal';
 import requests from "../api/requests";
 
 export default function Home() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelected] = useState({});
+
+  const movieClick = movie => {
+    setModalOpen(true);
+    setMovieSelected(movie);
+  };
+
   return (
     <>
-      <Navbar />
-      <Banner />
-      <Row
-        id="1"
-        title="오직 넷플릭스에서만"
-        fetchUrl={requests.fetchNetflixOriginals}
-        // isLargeRow
-      />
-      <Row id="2" title="지금 뜨는 콘텐츠" fetchUrl={requests.fetchTrending} />
-      <Footer />
+      <Banner movieClick={movieClick} />
+      <RowList>
+        <Row
+          id="1"
+          title="오직 넷플릭스에서만"
+          fetchUrl={requests.fetchNetflixOriginals}
+          movieClick={movieClick}
+        />
+        <Row
+          id="2"
+          title="지금 뜨는 콘텐츠"
+          fetchUrl={requests.fetchTrending}
+          movieClick={movieClick}
+        />
+      </RowList>
+      {modalOpen && (
+        <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
+      )}
     </>
   );
 };
+
+const RowList = styled.div`
+  width: 100%;
+  background-color: #000;
+`;
