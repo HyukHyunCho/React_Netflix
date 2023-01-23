b1eb917a083401287eeda0b836f9c2d7364c4487
HyukHyunCho <gurgus0189@gmail.com>
2023-01-24 03:12:24 +0900

    Update Style

--- src/components/Empty/Empty.jsx
+++ src/components/Empty/Empty.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 export default function Empty({ children }) {
-  // "${}";
+ 
   return (
     <Layout>
       <Container>
@@ -33,6 +33,7 @@ const Layout = styled.div`
   align-items: center;
   width: 100%;
   height: 100vh;
+  background-color: #000;
 `;
 const Container = styled.div`
   font-size: 12px;
