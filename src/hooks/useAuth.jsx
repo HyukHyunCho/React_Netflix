93b60633439c78fa0fb21ac479ffbaa70956af6e
HyukHyunCho <gurgus0189@gmail.com>
2023-02-02 23:03:50 +0900

    Feat: 유저 get, put useQuery, useMutation 추가

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -3,21 +3,26 @@ import {
   userLogin,
   userSignup,
   imageUpload,
-  getUserImage,
+  userUpdate,
+  getUserInfo,
 } from "../services/axios";
 
 export const useLogin = () => {
   return useMutation(userInfo => userLogin(userInfo));
-}
+};
   
 export const useAddUser = () => {
   return useMutation(userInfo => userSignup(userInfo));
-}
+};
 
 export const useImageUpload = () => {
   return useMutation(formImg => imageUpload(formImg));
-}
+};
 
-export const useImage = (filepath) => {
-  return useQuery(["image"], () => getUserImage(filepath));
-}
+export const useUpdateUser = () => {
+  return useMutation(userInfo => userUpdate(userInfo));
+};
+
+export const useUserInfo = (accountName) => {
+  return useQuery(["userInfo"], () => getUserInfo(accountName));
+};
