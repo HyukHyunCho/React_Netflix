a42d1e77a6d2a184e9d121616cebd7a50ebebfd9
HyukHyunCho <gurgus0189@gmail.com>
2023-02-12 17:19:59 +0900

    Feat: 검색 리스트 모달 추가

--- src/components/Row/RowSearch.jsx
+++ src/components/Row/RowSearch.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { RowSearchItem } from "./styles";
 import "react-multi-carousel/lib/styles.css";
 
-export default function RowSearch({ movie }) {
+export default function RowSearch({ movie, movieClick }) {
   return (
     <RowSearchItem
       key={movie.id}
       alt={movie.name}
       src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+      onClick={() => movieClick(movie)}
     />
   );
 }
